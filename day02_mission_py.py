2d33852778b954faf49f8170c7283facf219619e
sooox0809 <sooox89@gmail.com>
2023-01-13 17:35:01 +0900

    mission 02

--- day02_mission_py.py
+++ day02_mission_py.py
@@ -3,15 +3,28 @@
 
 import random
 
-secret = random.randint(1,10)
-# 1<= secret <=10 randint함수 랜덤한 정수(int) 반환
-guess = random.randint(1,10)
-if secret > guess :
-    print ('too low')
-elif secret < guess :
-    print ('too high')
-elif secret ==guess :
-    print ('just right')
+secret = random.randint(1, 10)
+# 1<= secret <=10
+# randint 함수 랜덤한 정수(int) 반환
+guess = random.randint(1, 10)
+if secret > guess:
+    print('too low')
+elif secret < guess:
+    print('too high')
+elif secret == guess:
+    print('just right')
 
+# mission_02
 
-
+small = True
+green = True
+if small:
+    if green:
+        print("pea")
+    else:
+        print("cherry")
+else:
+    if green:
+        print("watermelon")
+    else:
+        print("pumpkin")
