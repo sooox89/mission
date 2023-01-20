7082cd8abe204daf6e066697e301c091a77eb34e
sooox0809 <sooox89@gmail.com>
2023-01-20 18:31:02 +0900

    day07 mission ex.10-4

--- day07 mission.py
+++ day07 mission.py
@@ -1,17 +1,9 @@
 #ch10
-#ex.10-3
-class Thing3():
-    def __init__(self,letters):
-        self.letters = letters
-letters = Thing3('xyz')
-print(letters.letters)
-
-# class Thing2():
-#     def __init__(self,letters):
-#         self.letters = letters
-# 
-#
-# class Thing3(Thing2):
-#     pass
-# letters = Thing3('xyz')
-# print(letters.letters)
+#ex.10-4
+class Element():
+    def __init__(self,name,symbol,number):
+        self.name = name
+        self.symbol = symbol
+        self.number = number
+ob = Element('Hydrogen','H',1)
+print(ob.name,ob.symbol,ob.number)
