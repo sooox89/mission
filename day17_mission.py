8920e09d716a0338258c86b424673b998e9404da
sooox0809 <skycasey@inha.edu>
2023-02-07 20:37:09 +0900

    day 17 mission ch7 큐 응용예제 02

--- day17_mission.py
+++ day17_mission.py
@@ -1,21 +1,14 @@
 def is_queue_full():
     global front, rear, SIZE, queue
-    if rear != SIZE-1:
-        return False
-    elif rear == SIZE-1 and front == -1:
-        print("큐가 꽉 찬 상태다 !")
+    if (rear+1)%SIZE == front:
+        print("큐가 꽉 찼습니다")
         return True
-    else:
-        for i in range(front+1,SIZE):
-            queue[i-1]=queue[i]
-            queue[i]=None
-        front-=1
-        rear-=1
-        return False
+    return False
 
 def is_queue_empty():
     global front, rear, SIZE, queue
-    if rear==-1 and front==-1:
+    if front == rear:
+        print("큐가 비었습니다")
         return True
     return False
 
@@ -23,51 +16,47 @@ def en_queue(data):
     global front, rear, SIZE, queue
     if (is_queue_full()):
         return
-    else:
-        rear+=1
-        queue[rear] = data
+    rear=(rear+1)%SIZE
+    queue[rear] = data
+
 
 def de_queue():
     global front, rear, SIZE, queue
     if(is_queue_empty()):
         return
-    else:
-        front+=1
-        data=queue[front]
-        queue[front]=None
-        return data
-        print(data)
-
-        for i in range(front+1,rear+1):
-            queue[i-1]=queue[i]
-            queue[i]=None
-        front-=1
-        rear-=1
+    front=(front+1)%SIZE
+    data=queue[front]
+    queue[front]=None
+    return data
 
 def peek():
     global front, rear, SIZE, queue
     if(is_queue_empty()):
         print("큐가 비어있습니다")
         return None
-    else:
-        return queue[front+1]
+    return (queue[(front+1)%SIZE])
+
+def estimate_time():
+    global front, rear, SIZE, queue
+    time_sum = 0
+    for i in range((front+1)%SIZE, (rear+1)%SIZE):
+        time_sum += queue[i][1]
+    return time_sum
 
 
 # 전역변수 선언
-SIZE = 5
+SIZE = 6
 queue = [None for _ in range(SIZE)]
-front = rear = -1
+front = rear = 0
 
 if __name__=="__main__":
-    en_queue("태양")
-    en_queue("지디")
-    en_queue("대성")
-    en_queue("탑")
-    en_queue("수경")
-    print("대기 줄 상태 : ",queue)
+    wait_call= [['사용',10],['고장',5],['환불',3],['환불',3],['고장',5]]
 
-    for i in range(rear+1):
-        print(de_queue(),"님 식당에 들어감")
-        print("대기 줄 상태 : ",queue)
+    for call in wait_call:
+        print("귀하의 대기 예상 시간은", estimate_time(), "분입니다.")
+        print("현재 대기 콜-->", queue)
+        en_queue(call)
+        print()
 
-    print("대기 줄 없음!")
+    print("최종 대기 콜--> ", queue)
+    print("프로그램 종료!")
