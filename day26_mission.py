5aa3e6e985c5214135dc818d33f284fe7db8bca7
sooox0809 <skycasey@inha.edu>
2023-02-21 01:19:29 +0900

    day 26 응용예제 2

--- day26_mission.py
+++ day26_mission.py
@@ -1,42 +1,52 @@
-## 함수 선언 부분 ##
+import random
 
-def binary_search(array, find_data):
+#클래스와 함수 선언 부분#
+def seq_search(array, f_data):
+    global  count
+    pos = -1
+    for i in range(len(array)):
+        count += 1
+        if array[i] == find_data:
+            pos= i
+            break
+    return pos
+
+def bin_search(array,f_data):
+    global count
     start = 0
-    end = len(array) - 1
+    end = len(array) -1
 
-    while (start <= end):
-        mid = (start + end) // 2
-        if find_data == array[mid]:
-            return array[mid]
-        elif find_data > array[mid]:
-            start = mid + 1
+    while(start <= end):
+        count +=1
+        mid = (start + end) //2
+
+        if f_data == array[mid]:
+            return mid
+        elif f_data > array[mid]:
+            start = mid +1
         else:
-            end = mid - 1
+            end = mid -1
 
-    return -1
+# 전역 변수 선언 부분 #
 
+data_array ,sorted_array = [] ,[]
+find_data = 7878
+count = 0
 
-## 전역 변수 선언 부분 ##
-product_sold = ['코카콜라', '츄파춥스', '도시락', '삼각김밥', '레쓰비캔커피', '츄파춥스', '도시락', '츄파춥스',
-                '레쓰비캔커피', '코카콜라', '코카콜라', '도시락', '코카콜라', '삼각김밥', '삼각김밥', '바나나맛 우유', '삼각김밥', '츄파춥스', '바나나맛 우유', '코카콜라']
-product_type = []
+# 메인 코드 부분 #
+data_array = [random.randint(0,999999) for _ in range(1000000)]
+data_array.insert(random.randint(0,1000000), find_data)
+sorted_array = sorted(data_array)
 
-## 메인 코드 부분 ##
-print(f'#오늘 판매된 전체 물건(중복O, 정렬X) --> {product_sold}')
-product_sold.sort()
-print(f'#오늘 판매된 전체 물건(중복O, 정렬O) --> {product_sold}')
-product_type = list(set(product_sold))
-print(f'#오늘 판매된 물품 종류(중복X) --> {product_type}')
+print(f'#비정렬 배열(100만개) --> {data_array[0:5]} ~~~~ {data_array[-5:len(data_array)]}')
+print(f'#정렬 배열(100만개) --> {sorted_array[0:5]} ~~~~ {sorted_array[-5:len(sorted_array)]}')
 
-count_list = []
-for product in product_type:
-    count = 0
-    position = 0
-    while position != -1:
-        position = binary_search(product_sold, product)
-        if position != -1:
-            count += 1
-            del(product_sold[position])
-    count_list.append((product, count))
-print()
-print(f'결산 결과 --> {count_list}')
+count = 0
+pos = seq_search(data_array,find_data)
+if pos != -1:
+    print(f'순차 검색(비정렬 데이터) --> {count}회 ')
+
+count = 0
+pos = bin_search(sorted_array,find_data)
+if pos != -1:
+    print(f'이진 검색(정렬 데이터) --> {count}회')
